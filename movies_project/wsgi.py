1f673325fb42465fb5805b85f243e3dc4223e15b
Muhammad Ridha Anshari <mridhaans@gmail.com>
2018-11-25 10:59:44 +0700

    Adding some requirements

--- movies_project/wsgi.py
+++ movies_project/wsgi.py
@@ -11,6 +11,12 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movies_project.settings')
 
 application = get_wsgi_application()
+
+# Use whitenoise package to serve static files on Heroku
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
