6b1cb6ae2e6cdde1570c6d0284ef4d8d4c6b8e41
Muhammad Ridha Anshari <mridhaans@gmail.com>
2018-11-25 11:57:28 +0700

    Redirect index to movies

--- movies_project/urls.py
+++ movies_project/urls.py
@@ -22,5 +22,5 @@ from django.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('movies_api.devUrls')),
-    path('/',include('movies_api.devUrls')),
+    path('',include('movies_api.devUrls')),
 ]
